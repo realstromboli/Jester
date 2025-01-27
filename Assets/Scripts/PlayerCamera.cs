656410809ed335d2080eb5680f802a9c6422c91d
realstromboli <146856808+realstromboli@users.noreply.github.com>
2025-01-27 18:10:37 -0500

    Stuff

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -15,28 +15,39 @@ public class PlayerCamera : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    public GameManager gmScript;
+
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+
+        if (gmScript.isGameActive == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        
     }
 
     
     void Update()
     {
-        //mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+        if (gmScript.isGameActive == true)
+        {
+            //mouse input
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
+            yRotation += mouseX;
+            xRotation -= mouseY;
 
-        //rotate no more than 90 degrees
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            //rotate no more than 90 degrees
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //rotates camera and player orientation
-        camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-        playerObj.rotation = Quaternion.Euler(0, yRotation, 0);
+            //rotates camera and player orientation
+            camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+            playerObj.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+        
     }
 }
