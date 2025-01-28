bb111dc4155254b7ae0c96b7e82b88317f507025
realstromboli <146856808+realstromboli@users.noreply.github.com>
2025-01-28 13:54:58 -0500

    l

--- Assets/Scripts/DataPersistence/DataPersistenceManager.cs
+++ Assets/Scripts/DataPersistence/DataPersistenceManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class DataPersistenceManager : MonoBehaviour
 {
     
+    private GameData gameData;
+
     public static DataPersistenceManager instance
     {
         get; private set;
@@ -30,4 +32,28 @@ public class DataPersistenceManager : MonoBehaviour
         }
         instance = this;
     }
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        // TODO - Load any saved data from a raw file using data handler
+        // if no data loads, initialize new game
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing data to defaults");
+            NewGame();
+        }
+        // TODO - push the loaded data to all other scripts that need it
+    }
+
+    public void SaveGame()
+    {
+        // TODO - pass data to other scripts for updates
+        // TODO - save data to a file using data handler
+    }
+
 }
