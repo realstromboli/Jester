801ead811e32dbd002748f8b8c9ef2b287f5395d
realstromboli <146856808+realstromboli@users.noreply.github.com>
2025-02-14 10:49:43 -0500

    Stuff

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+}
