44e1aef6e95b2cedf95c4e2eaf139f6d00e40c58
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2025-02-21 12:32:34 -0500

    Allow dialogue to puase

--- Assets/Scripts/Jonathan Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/Jonathan Scripts/DialogueSystem/DialogueManager.cs
@@ -16,12 +16,15 @@ public class DialogueManager : MonoBehaviour
     private Animator anim;
     private Coroutine typing;
 
+    private GameManager gameManager;
+
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
             anim = GetComponent<Animator>();
+            gameManager = FindObjectOfType<GameManager>();
         }
         else
         {
@@ -68,6 +71,50 @@ public class DialogueManager : MonoBehaviour
 
     private IEnumerator WaitAndReadNext(string text)
     {
+        float waitTime = (dialogueTypeSpeed * text.Length) + dialogueDelay;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < waitTime)
+        {
+            // Pause if the game is not active
+            while (!gameManager.isGameActive)
+            {
+                yield return null;
+            }
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        ReadNext();
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        dialogue.text = "";
+        int index = 0;
+
+        while (index < text.Length)
+        {
+            // Pause if the game is not active
+            while (!gameManager.isGameActive)
+            {
+                yield return null;
+            }
+
+            dialogue.text += text[index];
+            index++;
+            yield return new WaitForSeconds(dialogueTypeSpeed);
+        }
+
+        typing = null;
+
+        StartCoroutine(WaitAndReadNext(text));
+    }
+
+    /*
+    private IEnumerator WaitAndReadNext(string text)
+    {
         yield return new WaitForSeconds((dialogueTypeSpeed * text.Length) + dialogueDelay);
         ReadNext();
     }
@@ -94,4 +141,5 @@ public class DialogueManager : MonoBehaviour
 
         StartCoroutine(WaitAndReadNext(text));
     }
+    */
 }
