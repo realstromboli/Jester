d01a334c980ed41ef469581638093604bc4d7cc0
realstromboli <146856808+realstromboli@users.noreply.github.com>
2025-05-07 18:23:55 -0400

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -639,7 +639,7 @@ public class GameManager : MonoBehaviour, IDataPersistence
         if (string.IsNullOrEmpty(sceneName))
         {
             Debug.LogWarning("Scene name is empty, defaulting to 'Inside Trailer'");
-            sceneName = "TUT";
+            sceneName = "StartScreen";
         }
 
         // Load the saved scene
