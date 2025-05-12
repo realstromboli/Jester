1d5660d53d6d7489ddab0ed73c5c3bd28deea4fd
realstromboli <146856808+realstromboli@users.noreply.github.com>
2025-05-12 17:29:31 -0400

    a

--- Assets/Scripts/Jonathan Scripts/DialogueSystem/QuestionDialogueTrigger.cs
+++ Assets/Scripts/Jonathan Scripts/DialogueSystem/QuestionDialogueTrigger.cs
@@ -28,7 +28,7 @@ public class QuestionDialogueTrigger : MonoBehaviour
         pmScript = GameObject.Find("Player").GetComponent<PlayerMovement>();
         dialogueManager = GameObject.Find("DialogueBox").GetComponent<DialogueManager>();
 
-        if (dialogueManager.dialogueViewedSave >= 5)
+        if (dialogueManager.dialogueViewedSave >= 6)
         {
             objectRenderer.material = newMaterial;
             dtScript = GameObject.Find("HiddenDialogueSpeaker").GetComponent<DialogueTrigger>();
