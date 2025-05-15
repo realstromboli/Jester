f6cc6bfea205d72e1fb6b34d6a3551bd41382331
JA-FSU <78230541+JA-FSU@users.noreply.github.com>
2025-05-15 13:09:16 -0400

    Finished maze 1 and tutorial

--- Assets/Scripts/ReactiveReticle.cs
+++ Assets/Scripts/ReactiveReticle.cs
@@ -51,7 +51,8 @@ public class ReactiveReticle : MonoBehaviour
             }
             else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("JustShowHand") ||
                      hit.collider.gameObject.layer == LayerMask.NameToLayer("Ghost") ||
-                     hit.collider.gameObject.layer == LayerMask.NameToLayer("tPoster"))
+                     hit.collider.gameObject.layer == LayerMask.NameToLayer("tPoster") ||
+                hit.collider.gameObject.layer == LayerMask.NameToLayer("InverseInteractable"))
             {
                 reticleHandSprite.enabled = true;
                 reticleTalkSprite.enabled = false;
